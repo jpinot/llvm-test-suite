8610e96ef8415ce62653c7a2147afa4f18cfc0de
Laurent Thévenoux <lthevenoux@kalray.eu>
2019-08-08 17:07:47 +0200

    [k1c] Initial testsuite settings
    Summary: This patch allows to build and run the supported subset of the suite.
    
    Test Plan: n/a
    
    Reviewers: O81 llvm-project!
    
    Differential Revision: https://phab.kalray.eu/D1533

--- cmake/modules/DetectArchitecture.c
+++ cmake/modules/DetectArchitecture.c
@@ -6,6 +6,8 @@ const char *str = "ARCHITECTURE IS ARM";
 const char *str = "ARCHITECTURE IS Alpha";
 #elif defined(__loongarch__)
 const char *str = "ARCHITECTURE IS LoongArch";
+#elif defined(__k1c__)
+const char *str = "ARCHITECTURE IS K1C";
 #elif defined(__mips__)
 const char *str = "ARCHITECTURE IS Mips";
 #elif defined(__powerpc__) || defined(__ppc__) || defined(__power__)
